d97d8e05af3b199896b7fe80e2cfd1da8fba8285
Akshata-IITR <akshatagaikwad13@gmail.com>
2026-06-09 22:40:58 +0530

    Time: 1484 ms (29.56%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,6 @@
+SELECT customer_id, count(*) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions
+ON Visits.visit_id = Transactions.visit_id
+WHERE transactions.transaction_id IS NULL
+GROUP BY customer_id
